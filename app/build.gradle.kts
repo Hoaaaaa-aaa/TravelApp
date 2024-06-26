1181d61fd0cd7b95944bfa9a19516962f70d2f43
thutrang <lethutrang2k3mh@gmail.com>
2024-06-26 15:00:42 +0700

    <Trang completed>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     alias(libs.plugins.androidApplication)
     id("com.google.gms.google-services")
@@ -45,4 +47,7 @@ dependencies {
     implementation("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.google.firebase:firebase-auth:21.0.1")
+    implementation("com.google.firebase:firebase-database:20.0.0")
+
 }
